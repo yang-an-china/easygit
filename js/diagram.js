cefcc515ca4d5ed267cdb216f22d1ffa8f7de724
yang-an-china <yang_an19861217@126.com>
2019-12-28 12:32:30 +0800

    V1

--- js/diagram.js
+++ js/diagram.js
@@ -1,9 +1,18 @@
-function get_gitgraph(id) {
+function get_gitgraph(id, hash) {
+    hash = arguments[1] ? arguments[1] : false;
     var graphContainer = document.getElementById(id);
     var gitTemplate = GitgraphJS.templateExtend(GitgraphJS.TemplateName.Metro, {
-                                              commit: {message: {displayAuthor: false, displayHash: false}},
+                                              commit: {message: {displayAuthor: false, displayHash: hash}},
                                               branch: {lineWidth: 7}});
     var gitgraph = GitgraphJS.createGitgraph(graphContainer, {template: gitTemplate, branchLabelOnEveryCommit: true});
     return gitgraph;
 }
 
+function get_gitgraph_H(id) {
+    var graphContainer = document.getElementById(id);
+    var gitTemplate = GitgraphJS.templateExtend(GitgraphJS.TemplateName.Metro, {
+                                              commit: {message: {displayAuthor: false, displayHash: false}},
+                                              branch: {lineWidth: 7, spacing:90}});
+    var gitgraph = GitgraphJS.createGitgraph(graphContainer, {template: gitTemplate, orientation:"horizontal", mode:"compact"});
+    return gitgraph;
+}
